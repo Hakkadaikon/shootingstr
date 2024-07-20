e5c594c04102fb52228310b4d36619b22d5691b1
hakkadaikon <hakkadaikon@yahoo.co.jp>
2024-07-21 04:20:26 +0900

    Add json analyze

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
  */
 
 #include "websockets/websockets.h"
+#include "utils/compatibility.h"
 #include <yyjson.h>
 
 int user_callback(
@@ -12,10 +13,56 @@ int user_callback(
     const int      write_buffer_len,
     unsigned char* write_buffer)
 {
-    websocket_printf("test user_callback\n");
     websocket_printf("user_callback data : [%s]", data);
 
-    snprintf((char*)write_buffer, write_buffer_len, "test reply");
+    size_t      len = strlen(data);
+    yyjson_doc* doc = yyjson_read(data, len, 0);
+    if (doc == NULL) {
+        websocket_printf("yyjson_read failed.\n");
+        goto FINALIZE;
+    }
+
+    yyjson_val* root = yyjson_doc_get_root(doc);
+    if (!yyjson_is_arr(root)) {
+        websocket_printf("yyjson_get_root failed.\n");
+        goto FINALIZE;
+    }
+
+    yyjson_val* event_type_obj = yyjson_arr_get(root, 0);
+    if (!yyjson_is_str(event_type_obj)) {
+        websocket_printf("event type is not str.\n");
+        goto FINALIZE;
+    }
+
+    yyjson_val* sub_id_obj = yyjson_arr_get(root, 1);
+    if (!yyjson_is_str(sub_id_obj)) {
+        websocket_printf("sub_id is not str.\n");
+        goto FINALIZE;
+    }
+
+    const char* event_type = yyjson_get_str(event_type_obj);
+    const char* sub_id     = yyjson_get_str(sub_id_obj);
+
+    if (strstr(event_type, "EVENT")) {
+        yyjson_val* event_data = yyjson_arr_get(root, 2);
+        if (!yyjson_is_obj(event_data)) {
+            websocket_printf("EVENT data is not json\n");
+            goto FINALIZE;
+        }
+
+        // Send OK message
+        snprintf((char*)write_buffer, write_buffer_len, "[\"OK\",\"%s\",true,\"\"]", sub_id);
+        goto FINALIZE;
+    } else if (strstr(event_type, "CLOSE")) {
+        // Send EOSE message
+        snprintf((char*)write_buffer, write_buffer_len, "[\"EOSE\",\"%s\"]", sub_id);
+        goto FINALIZE;
+    }
+
+FINALIZE:
+    if (doc != NULL) {
+        yyjson_doc_free(doc);
+    }
     return 0;
 }
 
